ab64b11ba60f347c0f2547e5fe2c5628c14d8016
Antonio Cunanan <antonio.cunanan@gmail.com>
2023-01-11 21:51:28 -0600

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<h1>Hello World</h1>
+			</React.Fragment>
+		)
+	}
+}
+
+export default App
